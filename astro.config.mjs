412d5fef3ec925819cbcdd6e66910bb81f776c9c
alexi-ae <alexi.acunaespino@gmail.com>
2024-10-03 00:44:43 -0500

    update config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,10 +11,22 @@ import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
 
 import svelte from "@astrojs/svelte";
 
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http:localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://alexi-ae.github.io";
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+if(isBuild){
+  BASE_URL = LIVE_URL
+}
+
+
 // https://astro.build/config
 export default defineConfig({
+  server:{port:SERVER_PORT},
 //  site: "http://localhost:4321/",
-  site: "https://alexi-ae.github.io",
+  site: BASE_URL,
   integrations: [
     sitemap(),
     robotsTxt({
